ed71ee1aad7cca58e8af701d6ea6fd3166b12fdf
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-04-12 16:23:12 -0300

    deleted postType - useless controller

--- api/src/routes/typeRouter.js
+++ api/src/routes/typeRouter.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { getTypes, postType } = require('../controllers');
+const { getTypes } = require('../controllers');
 
 const typeRouter= express.Router();
 
 typeRouter.get("/types", getTypes);
-typeRouter.post("/types", postType);
 
 module.exports = typeRouter;

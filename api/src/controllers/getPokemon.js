28a7ae72abc614545c9b1fac12b2ebc6cd65939c
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-04-12 11:39:29 -0300

    Update getPokemon.js

--- api/src/controllers/getPokemon.js
+++ api/src/controllers/getPokemon.js
@@ -1,17 +1,14 @@
-const { Pokemon, Type } = require("../db.js");
+const axios = require("axios");
 
+
+//Este controller solicita hasta 1000 pokemones de la api
 const getPokemon = async (req, res) => {
     try {
-        const pokemons = await Pokemon.findAll({
-            attributes: ['id', 'apiId', 'name', 'image', 'hp', 'attack', 'defense', 'speed'],
-            include: {
-                model: Type,
-                attributes: ['name'],
-                through: {
-                    attributes: []
-                }
-            }
-        });
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=1000')
+        const pokemons = response.data.results.map(pokemon => ({
+            name: pokemon.name,
+            url: pokemon.url
+        }));
         res.status(200).json(pokemons);
     } catch (error) {
         res.status(500).json(error.message);

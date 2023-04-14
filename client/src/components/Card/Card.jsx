f4ae547b856349a3d4fbf512f8d31b5c49cb251e
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-04-14 01:08:21 -0300

    updated card.jsx (needs testing)

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,9 +5,9 @@ function Card({ name, image, type, types }) {
             <h2>{name}</h2>
             <img src={image} alt="" />
             <>{type? (<h2>{type}</h2>): null}</>
-            <>{types? types.map((type, index) => (
-                <h2 key={index}>{type.name}</h2>
-            )) : null}</>
+            <>{types? <h2>{(types.map((type) => (
+                <>{type.name}</>
+            )))}</h2> : null}</>
         </div>
     )
 }

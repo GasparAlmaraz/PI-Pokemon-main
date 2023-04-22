e96ed6cf40ae4c3bb1cc76e83ba3c1374ec8adf4
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-04-22 12:21:33 -0300

    Update Card.jsx

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 function Card({ id, name, image, type }) {
     return (
-        <div>
-            <h5>{id}</h5>
-            <h2>{name}</h2>
-            <Link to={`/detail/${id}`}>
-                <img src={image} alt=""/>
-            </Link>
-            <>{type ? <h2>{type.map(type => (
-                <> {type} </>
-            ))}</h2> : null}</>
-        </div>
+        <Link to={`/detail/${id}`}>
+            <div>
+                <h5>{id}</h5>
+                <h2>{name}</h2>
+                <img src={image} alt="" />
+                <>{type ? <h2>{type.map(type => (
+                    <> {type} </>
+                ))}</h2> : null}</>
+            </div>
+        </Link>
     )
 }
 

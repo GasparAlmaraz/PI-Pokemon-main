cda59c204840104cde0c6988d37f0fa4c3f0214d
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-05-18 20:39:25 -0300

    Update filter.js

--- api/src/controllers/utils/filter.js
+++ api/src/controllers/utils/filter.js
@@ -1,10 +1,9 @@
 const filterData = (query, array) => {
-
+    
     const filteredArray = array.filter(element => {
-        console.log(element);
-        // const keys = Object.keys(element.dataValues);
-        // return keys.some(key => element.dataValues[key] === query);
-    });
+        const keys = Object.keys(element.dataValues);
+        return keys.some(key => element.dataValues[key] === query);
+      });
     return filteredArray;
 };
 
